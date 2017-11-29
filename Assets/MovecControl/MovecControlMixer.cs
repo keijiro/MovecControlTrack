c678e8c7a83ca5097f686d9d1b6d401217983845
Keijiro Takahashi <keijiro@gmail.com>
2017-11-29 10:31:41 +0900

    Added movec generation mode property to MovecControl.

--- Assets/MovecControl/MovecControlMixer.cs
+++ Assets/MovecControl/MovecControlMixer.cs
@@ -7,7 +7,7 @@ class MovecControlMixer : PlayableBehaviour
 {
     #region Private state
 
-    int _enableCount;
+    int _delayCount;
     SkinnedMeshRenderer[] _targetCache;
     MaterialPropertyBlock _sheet;
 
@@ -17,47 +17,58 @@ class MovecControlMixer : PlayableBehaviour
 
     public override void OnGraphStart(Playable playable)
     {
-        _enableCount = 0;
+        _delayCount = 0;
         _targetCache = null;
         _sheet = new MaterialPropertyBlock();
     }
 
     public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
-        // Target root transform
+        // Retrieve the target root transform from the track-given data.
         var root = playerData as Transform;
         if (root == null) return;
 
-        // Get the target renderers and store them into a cache array.
+        // Update the cache that contains the target renderer list.
         if (_targetCache == null)
             _targetCache = root.GetComponentsInChildren<SkinnedMeshRenderer>();
 
-        // Check if the current time is covered by a clip.
+        // Scan the track clips to determine the current movec mode.
+        var mode = MotionVectorGenerationMode.Camera;
         var inputCount = playable.GetInputCount();
-        var totalWeight = 0.0f;
         for (var i = 0; i < inputCount; i++)
-            totalWeight += playable.GetInputWeight(i);
-
-        // Increment the counter if covered, or reset the counter.
-        if (totalWeight > 0) _enableCount++; else _enableCount = 0;
-
-        if (_enableCount > 2)
         {
-            // Enabled: Set per-object movec mode and reset the bias value.
+            if (playable.GetInputWeight(i) > 0)
+            {
+                var clip = (ScriptPlayable<MovecControlPlayable>)playable.GetInput(i);
+                mode = clip.GetBehaviour().mode;
+                break;
+            }
+        }
+
+        // We know that the per-object movec mode makes an undesirable spike in
+        // the first frame, so delay actual changes for two frames.
+        if (mode == MotionVectorGenerationMode.Object)
+        {
+            if (_delayCount++ < 2) mode = MotionVectorGenerationMode.Camera;
+        }
+        else
+        {
+            _delayCount = 0;
+        }
+
+        // Update the movec mode of the renderers.
+        foreach (var smr in _targetCache)
+            smr.motionVectorGenerationMode = mode;
+
+        // Cancel the depth bias with the per-object movec mode.
+        if (mode == MotionVectorGenerationMode.Object)
+        {
             foreach (var smr in _targetCache)
             {
-                smr.motionVectorGenerationMode = MotionVectorGenerationMode.Object;
-
                 smr.GetPropertyBlock(_sheet);
                 _sheet.SetFloat("_MotionVectorDepthBias", 0);
                 smr.SetPropertyBlock(_sheet);
             }
-        }
-        else
-        {
-            // Disabled: Set camera movec mode.
-            foreach (var smr in _targetCache)
-                smr.motionVectorGenerationMode = MotionVectorGenerationMode.Camera;
         }
     }
 
--- Assets/MovecControl/MovecControlPlayable.cs
+++ Assets/MovecControl/MovecControlPlayable.cs
@@ -4,4 +4,5 @@ using UnityEngine.Playables;
 [System.Serializable]
 public class MovecControlPlayable : PlayableBehaviour
 {
+    public MotionVectorGenerationMode mode = MotionVectorGenerationMode.Object;
 }
